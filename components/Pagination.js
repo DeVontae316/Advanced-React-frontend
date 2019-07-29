5db05511b539d776748029bd795bd7d2e233a0d3
DeVontae <DeVontae@DeVontaes-MacBook-Air.local>
2019-07-29 18:45:57 -0500

    Pagination added

---
+++ components/Pagination.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import PaginationStyles from './styles/PaginationStyles';
+import gql from 'graphql-tag';
+import {Query} from 'react-apollo';
+import {perPage} from '../config';
+import Head from 'next/head';
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const Center = styled.div`
+ text-align:center;
+ margin-top:8px;
+ a:hover{
+   cursor:alias;
+ }
+`
+
+const PAGINATION_QUERY = gql`
+query PAGINATION_QUERY{
+  itemsConnection{
+    aggregate{
+      count
+    }
+  }
+}
+`;
+
+const Pagination = props =>{
+  return(
+    <Query query={PAGINATION_QUERY}>
+     {({data,loading,error})=>{
+       const totalItems = data.itemsConnection.aggregate.count;
+       const page = props.page;
+       const pages = Math.ceil(totalItems / perPage);
+       if(loading)<p>...loading</p>
+       if(error)<p>{error.message}</p>
+       return<PaginationStyles>
+
+         <Link href={{pathname:"/items",page:page - 1}}>
+          <a aria-disabled={page <= 1}>Prev</a>
+         </Link>
+
+          <p>Page {props.page} out of {pages}</p>
+
+         <Link href={{pathname:"/items",page:page + 1}}>
+           <a aria-disabled={page >= pages}>Next</a>
+         </Link>
+
+          <p>{totalItems} items total</p>
+       </PaginationStyles>
+     }}
+    </Query>
+  )
+}
+
+
+export default Pagination;
