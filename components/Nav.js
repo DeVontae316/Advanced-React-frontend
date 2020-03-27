aab4b092584c49af1938912ce52f2b9a66c31e76
DeVontae <DeVontae@DeVontaes-MacBook-Air.local>
2020-03-27 09:01:45 -0500

    Used data from hook on button

--- components/Nav.js
+++ components/Nav.js
@@ -1,27 +1,77 @@
 import Link from 'next/link';
 import NavStyles from '../components/styles/NavStyles';
+import User from './User';
+import SignOut from './SignOut';
+import {Mutation} from 'react-apollo';
+
+import CartCount from './CartCount';
+import styled from 'styled-components';
+import Hooks from './Hooks';
+import {useCart} from  './LocalState';
+
+const Cart = styled.p`
+background:${props => props.theme.red};
+color:white;
+text-align:center;
+padding:1.3rem;
+line-height:2rem;
+min-width:3rem;
+margin-right:3rem;
+font-weight:100;
+font-feature-setting:'tnum';
+font-variant-numeric:tabular-numeric;
+
+`
 
 const Nav = (props)=>{
-  return(
-   <NavStyles>
-     <Link href='/items'>
-      <a>Shop</a>
-     </Link>
-     <Link href="/sell">
-       <a>Sell</a>
-     </Link>
-     <Link href='/signup'>
-      <a>SignUp</a>
-     </Link>
-     <Link href="/orders">
-       <a>Orders</a>
-     </Link>
-     <Link href="test">
-       <a>Account</a>
-     </Link>
+ const {toggleCart} = useCart();
 
-  </NavStyles>
-  )
-}
+    return <User>
+     {({data:{me}})=>(
+     
+      <NavStyles>
+
+       <Link href='/items'>
+        <a>Shop</a>
+
+       </Link>
+       {me &&(
+        <>
+
+         <Link href="/sell">
+           <a>Sell</a>
+         </Link>
+
+         <Link href="/orders">
+           <a>Orders</a>
+         </Link>
+         <Link href="test">
+           <a>Account</a>
+         </Link>
+         
+          <SignOut />
+           {me.name}
+          <button  onClick={toggleCart}type="button">Cart</button>
+            
+          
+        </>
+
+       )}
+       {!me &&(
+         <>
+         <Link href='/signup'>
+          <a>SignIn</a>
+         </Link>
+         </>
+       )}
+
+
+    </NavStyles>
+     )}
+
+    </User>
+
+  }
+
 
 export default Nav;
